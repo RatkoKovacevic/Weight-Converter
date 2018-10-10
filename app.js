49b58dd5dc50f6dd84eac5cf1e930d8ca8224efb
RatkoKovacevic <ratkokovacevic100@live.de>
2018-10-10 18:02:53 +0200

    initial

---
+++ app.js
@@ -0,0 +1,11 @@
+let hide = document.getElementById('output').style.visibility = 'hidden';
+
+let input = document.getElementById('input');
+
+input.addEventListener('input', function (e) {
+  document.getElementById('output').style.visibility = 'visible';
+  let lbs = e.target.value;
+  document.getElementById('gramsOutput').innerHTML = lbs / 0.0022046;
+  document.getElementById('kgOutput').innerHTML = lbs / 2.2046;
+  document.getElementById('ozOutput').innerHTML = lbs * 16;
+});
